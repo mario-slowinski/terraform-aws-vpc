0bfe172beed20723c2f54e42d4253e4acdf77578
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-06-01 20:03:43 +0200

    Add additional arguments.

--- vpc.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_vpc" "this" {
-  cidr_block = var.cidr_block
-
-  tags = merge(local.tags, var.tags, local.Name)
-}
--- vpc.vars.tf
+++ vpc.vars.tf
@@ -1,53 +1,71 @@
-variable "name" {
-  type        = string
-  description = "Resource's name."
-  default     = ""
-}
-
 variable "cidr_block" {
   type        = string
   description = "The IPv4 CIDR block for the VPC."
   default     = null
 }
 
-variable "address_space" {
-  type        = list(string)
-  description = "The list of address spaces used by the virtual network."
-  default     = []
-}
-
-variable "location" {
+variable "instance_tenancy" {
   type        = string
-  description = "Region to create resource."
-  default     = ""
-}
-
-variable "ddos_protection_plan" {
-  type = object({
-    id     = string
-    enable = bool
-  })
-  description = "Optional block."
-  default = {
-    id     = ""
-    enable = false
-  }
-}
-
-variable "dns_servers" {
-  type        = list(string)
-  description = "List of IP addresses of DNS servers."
+  description = "A tenancy option for instances launched into the VPC."
   default     = null
 }
 
-variable "edge_zone" {
+variable "ipv4_ipam_pool_id" {
   type        = string
-  description = "Specifies the Edge Zone within the Azure Region where this Virtual Network should exist."
+  description = "The ID of an IPv4 IPAM pool you want to use for allocating this VPC's CIDR."
   default     = null
 }
 
-variable "flow_timeout_in_minutes" {
+variable "ipv4_netmask_length" {
+  type        = string
+  description = "The netmask length of the IPv4 CIDR you want to allocate to this VPC."
+  default     = null
+}
+
+variable "ipv6_cidr_block" {
+  type        = string
+  description = "IPv6 CIDR block to request from an IPAM Pool."
+  default     = null
+}
+
+variable "ipv6_ipam_pool_id" {
+  type        = string
+  description = "IPAM Pool ID for a IPv6 pool."
+  default     = null
+}
+
+variable "ipv6_netmask_length" {
   type        = number
-  description = "The flow timeout in minutes for the Virtual Network, which is used to enable connection tracking for intra-VM flows."
+  description = "Netmask length to request from IPAM Pool."
+  default     = null
+}
+
+variable "ipv6_cidr_block_network_border_group" {
+  type        = string
+  description = "By default when an IPv6 CIDR is assigned to a VPC a default ipv6_cidr_block_network_border_group will be set to the region of the VPC."
+  default     = null
+}
+
+variable "enable_dns_support" {
+  type        = bool
+  description = "A boolean flag to enable/disable DNS support in the VPC."
+  default     = null
+}
+
+variable "enable_network_address_usage_metrics" {
+  type        = bool
+  description = "Indicates whether Network Address Usage metrics are enabled for your VPC."
+  default     = null
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC."
+  default     = null
+}
+
+variable "assign_generated_ipv6_cidr_block" {
+  type        = bool
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC."
   default     = null
 }
