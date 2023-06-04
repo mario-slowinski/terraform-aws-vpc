6153156e1bab52d9224c454eedf8d5cca458f575
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-06-04 18:19:30 +0200

    Set "gateway_id" in route:
    * to passed gateway_id or
    * to the one just created in this module or
    * skip if set to null

--- route_table.main.tf
+++ route_table.main.tf
@@ -23,12 +23,19 @@ resource "aws_route_table" "many" {
       carrier_gateway_id         = route.value.carrier_gateway_id
       core_network_arn           = route.value.core_network_arn
       egress_only_gateway_id     = route.value.egress_only_gateway_id
-      gateway_id                 = route.value.gateway_id
-      nat_gateway_id             = route.value.nat_gateway_id
-      local_gateway_id           = route.value.local_gateway_id
-      network_interface_id       = route.value.network_interface_id
-      transit_gateway_id         = route.value.transit_gateway_id
-      vpc_endpoint_id            = route.value.vpc_endpoint_id
+      gateway_id = try(
+        # if regex matches => use given gateway_id
+        regex("^igw-[0-9a-z]{17}$", route.value.gateway_id),
+        # if not => try to use the one created in this module
+        aws_internet_gateway.this[route.value.gateway_id].id,
+        # if not => set to null meaning other attribute should be used
+        null
+      )
+      nat_gateway_id       = route.value.nat_gateway_id
+      local_gateway_id     = route.value.local_gateway_id
+      network_interface_id = route.value.network_interface_id
+      transit_gateway_id   = route.value.transit_gateway_id
+      vpc_endpoint_id      = route.value.vpc_endpoint_id
       vpc_peering_connection_id = try(
         # if regex matches => use given vpc_peering_connection_id
         regex("^pcx-[0-9a-z]{17}$", route.value.vpc_peering_connection_id),
