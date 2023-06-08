2b7a01b61c5f7878027b1b5db3a72428cfa0d420
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-06-08 18:10:01 +0200

    Update documentation.

--- peering_connection.vars.tf
+++ peering_connection.vars.tf
@@ -1,9 +1,9 @@
 variable "peering_connections" {
   type = list(object({
-    auto_accept   = optional(bool)   # Accept the peering (both VPCs need to be in the same AWS account and region)."
-    peer_owner_id = optional(string) # The AWS account ID of the owner of the peer VPC."
-    peer_region   = optional(string) # The region of the accepter VPC of the VPC Peering Connection."
-    peer_vpc_id   = string           # The ID of the VPC with which you are creating the VPC Peering Connection."
+    auto_accept   = optional(bool)   # Accept the peering (both VPCs need to be in the same AWS account and region).
+    peer_owner_id = optional(string) # The AWS account ID of the owner of the peer VPC.
+    peer_region   = optional(string) # The region of the accepter VPC of the VPC Peering Connection.
+    peer_vpc_id   = string           # The ID of the VPC with which you are creating the VPC Peering Connection.
     accepter = optional(object({
       allow_remote_vpc_dns_resolution = optional(bool)
     }))
