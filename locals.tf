03b407d4454cc7e12db61d1052506a8bf231107b
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-08-10 23:38:23 +0200

    Add to locals:
    * "internet_gateways"
    * "nat_gateways"

--- locals.tf
+++ locals.tf
@@ -1,7 +1,9 @@
 locals {
-  Name     = { "Name" : var.name }
-  vpc      = one([for name, vpc in aws_vpc.name : vpc])
-  subnets  = { for name, subnet in aws_subnet.name : name => subnet }
-  eips     = { for name, eip in aws_eip.name : name => eip }
-  peerings = { for vpc, peering in aws_vpc_peering_connection.vpc : vpc => peering }
+  Name              = { "Name" : var.name }
+  vpc               = one([for name, vpc in aws_vpc.name : vpc])
+  eips              = { for name, eip in aws_eip.name : name => eip }
+  internet_gateways = { for name, internet_gateway in aws_internet_gateway.name : name => internet_gateway }
+  nat_gateways      = { for name, nat_gateway in aws_nat_gateway.name : name => nat_gateway }
+  peerings          = { for vpc, peering in aws_vpc_peering_connection.vpc : vpc => peering }
+  subnets           = { for name, subnet in aws_subnet.name : name => subnet }
 }
