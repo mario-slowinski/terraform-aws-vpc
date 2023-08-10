a2cc1043f73c2b0864ee3b1b49485005dc357026
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-08-10 21:35:22 +0200

    Update "outputs":
    * output whole VPC object, not just its id
    * output "eips"
    * output "peerings"

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "id" {
-  value       = local.vpc_id
-  description = "VPC id."
+output "vpc" {
+  value       = local.vpc
+  description = "Map of VPC attributes."
 }
 
 output "subnets" {
   value       = local.subnets
   description = "Map of VPC subnets."
+}
+
+output "eips" {
+  value       = local.eips
+  description = "Map of VPC eips."
+}
+
+output "peerings" {
+  value       = local.peerings
+  description = "Map of VPC peerings."
 }
