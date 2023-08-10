02bd4d2390707154b3f4e8e3522cb203bacdf3ac
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-08-10 21:34:49 +0200

    Add "peerings" to "locals".

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
-  Name    = { "Name" : var.name }
-  vpc_id  = one([for name, vpc in aws_vpc.name : vpc.id])
-  subnets = { for name, subnet in aws_subnet.name : name => subnet.id }
-  eips    = { for name, eip in aws_eip.name : name => eip.id }
+  Name     = { "Name" : var.name }
+  vpc      = one([for name, vpc in aws_vpc.name : vpc])
+  subnets  = { for name, subnet in aws_subnet.name : name => subnet.id }
+  eips     = { for name, eip in aws_eip.name : name => eip.id }
+  peerings = { for vpc, peering in aws_vpc_peering_connection.vpc : vpc => peering }
 }
