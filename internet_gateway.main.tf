710ad460b6c60a5cb8b4fdd9e88f750322b6cb95
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-08-10 23:40:53 +0200

    Add required "name" argyment to "internet_gateways" variable and rename resource.

--- internet_gateway.main.tf
+++ internet_gateway.main.tf
@@ -1,6 +1,6 @@
-resource "aws_internet_gateway" "this" {
-  count = var.internet_gateway != null ? 1 : 0
+resource "aws_internet_gateway" "name" {
+  for_each = { for internet_gateway in var.internet_gateways : internet_gateway.name => internet_gateway if internet_gateway.name != null }
 
-  vpc_id = local.vpc.id
-  tags   = merge(var.tags, var.internet_gateway)
+  vpc_id = coalesce(each.value.vpc_id, local.vpc.id)
+  tags   = merge(var.tags, local.Name, each.value.tags, { Name = each.key })
 }
--- internet_gateway.vars.tf
+++ internet_gateway.vars.tf
@@ -1,5 +1,9 @@
-variable "internet_gateway" {
-  type        = map(string)
-  description = "Map of tags, create internet gateway if not null."
-  default     = null
+variable "internet_gateways" {
+  type = list(object({
+    name   = string                # Internet gateway name
+    vpc_id = optional(string)      # VPC id
+    tags   = optional(map(string)) # A map of tags to assign to the resource.
+  }))
+  description = "List of Internet Gateways."
+  default     = [{ name = null }]
 }
