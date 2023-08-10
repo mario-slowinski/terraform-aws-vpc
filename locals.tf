e0aedad05af08b43f157b991c983dfd0ba3de354
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-08-10 19:01:19 +0200

    Add "subnets" to "locals" and "outputs".

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
-  Name   = { "Name" : var.name }
-  vpc_id = one([for name, vpc in aws_vpc.name : vpc.id])
+  Name    = { "Name" : var.name }
+  vpc_id  = one([for name, vpc in aws_vpc.name : vpc.id])
+  subnets = { for name, subnet in aws_subnet.name : name => subnet.id }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "id" {
   value       = local.vpc_id
-  description = "VPC id"
+  description = "VPC id."
+}
+
+output "subnets" {
+  value       = local.subnets
+  description = "Map of VPC subnets."
 }
