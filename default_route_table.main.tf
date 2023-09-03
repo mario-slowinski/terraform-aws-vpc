bba3f529edfd751ad5e1984de41fde66bc2afdf6
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-09-03 18:36:36 +0200

    Use VPC name as index for "default_route_table".

--- default_route_table.main.tf
+++ default_route_table.main.tf
@@ -1,7 +1,7 @@
 resource "aws_default_route_table" "vpc" {
   for_each = {
     for route_table in var.route_tables :
-    local.vpc.id => route_table
+    coalesce(var.name, local.vpc.id) => route_table
     if coalesce(route_table.default, false)
   }
   default_route_table_id = local.vpc.default_route_table_id
